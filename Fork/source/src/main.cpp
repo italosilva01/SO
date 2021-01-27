007fa34728de3ce257c3bfc9199dec2603bc835d
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2021-01-27 12:05:56 -0300

    fork: configurar Cmake
    O exercutável "main" está na pasta "build"

--- Fork/source/src/main.cpp
+++ Fork/source/src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(){
-    std::cout<<"TATATE"<<std::endl;
-    
+    std::cout<<"TATPROQUITOTE"<<std::endl;
+
+    // pid_t pid;
+    // pid = fork();
+
+    // if(pid < 0){
+    //     exit(-1);
+    // }   
     return 0;
 }
